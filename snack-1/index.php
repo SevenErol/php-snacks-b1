eb3e33627b25452b104abec73b7839d09a286088
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-28 12:14:40 +0100

    add snack 1

--- snack-1/index.php
+++ snack-1/index.php
@@ -3,31 +3,43 @@ Ogni array avrà una squadra di casa e una squadra ospite,
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
 Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
+
+<!-- foreach (iterable_expression as $value)
+    statement
+foreach (iterable_expression as $key => $value)
+    statement -->
 <?php
 
-$teams = [
-    [
-        "home" => "Milano",
-        "visitor" => "Cantù",
-        "homePoints" => 85,
-        "visitorPoints" => 76
-    ],
-    [
-        "home" => "Milano",
-        "visitor" => "Cantù",
-        "homePoints" => 85,
-        "visitorPoints" => 76
-    ],
-    [
-        "home" => "Milano",
-        "visitor" => "Cantù",
-        "homePoints" => 85,
-        "visitorPoints" => 76
-    ],
-    [
-        "home" => "Milano",
-        "visitor" => "Cantù",
-        "homePoints" => 85,
-        "visitorPoints" => 76
-    ]
+$matches = [
+    'Milano - Cantu / 72-63',
+    'Milano - Cantu / 72-63',
+    'Milano - Cantu / 72-63',
+    'Milano - Cantu / 72-63'
 ];
+
+foreach ($matches as $element) {
+
+    echo $element;
+    echo '<br>';
+};
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
