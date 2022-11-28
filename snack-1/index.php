59fddc32f722ff4e5e88dcdcdab50daf81af7c18
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-28 12:03:21 +0100

    created array

---
+++ snack-1/index.php
@@ -0,0 +1,33 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+Ogni array avrà una squadra di casa e una squadra ospite, 
+punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+<?php
+
+$teams = [
+    [
+        "home" => "Milano",
+        "visitor" => "Cantù",
+        "homePoints" => 85,
+        "visitorPoints" => 76
+    ],
+    [
+        "home" => "Milano",
+        "visitor" => "Cantù",
+        "homePoints" => 85,
+        "visitorPoints" => 76
+    ],
+    [
+        "home" => "Milano",
+        "visitor" => "Cantù",
+        "homePoints" => 85,
+        "visitorPoints" => 76
+    ],
+    [
+        "home" => "Milano",
+        "visitor" => "Cantù",
+        "homePoints" => 85,
+        "visitorPoints" => 76
+    ]
+];
